f22851fad4814647c9ed7cfe0eebd27d1c32ee59
Sam Enoka <slwenoka@gmail.com>
2017-10-14 19:46:34 +0200

    Check for require to allow in browser dev

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { render } from 'react-dom'
 import Main from './Main'
 
-const {ipcRenderer} = window.require('electron')
+if (window.require) { // Check this so we can view app in browser
+  const {ipcRenderer} = window.require('electron')
 
-document.addEventListener('DOMContentLoaded', () => {
-  let n = new Notification('You did it!', {
-    body: 'Nice work.'
+  document.addEventListener('DOMContentLoaded', () => {
+    let n = new Notification('You did it!', {
+      body: 'Nice work.'
+    })
+
+    // Tell the notification to show the menubar popup window on click
+    n.onclick = () => { ipcRenderer.send('show-window') }
   })
-
-  // Tell the notification to show the menubar popup window on click
-  n.onclick = () => { ipcRenderer.send('show-window') }
-})
+}
 
 render(<Main />, document.querySelector('#main'))
